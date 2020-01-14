13c6875475a944df6a0ba64aeef8f19dd500cde9
kunaia <alekoxanta@gmail.com>
2020-01-14 19:00:37 +0400

    script to get name of public class from java code

---
+++ java/GetJavaPublicClassName.py
@@ -0,0 +1,23 @@
+from subprocess import Popen
+
+
+err = open('error.txt', 'w+')
+
+p = Popen(['javac', 'Main.java'], stderr=err)
+p.wait()
+
+err.close()
+
+#print (p.returncode)	
+if p.returncode != 0:
+	with open('error.txt', "r") as f:
+		t = f.readline()
+		s = "should be declared in a file named"
+		pos = t.find(s)
+		if pos == -1:
+			print("Compilation Error")  	# this happens if we did not found message "should be
+							# declared in a file named" in first line of error message
+		else:
+			new_name = t[pos+len(s)+1:].strip()
+			print("File should be named:", new_name)
+
